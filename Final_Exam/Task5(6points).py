6e4703bbfa2612ec6d91238f804aa6fc6a444d69
Annablaa <128103799+Annablaa@users.noreply.github.com>
2023-07-12 01:38:07 +0400

    hsh

---
+++ Final_Exam/Task5(6points).py
@@ -0,0 +1,34 @@
+""" Task 5 [6 points]"""
+
+"""
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. 
+Write a function climbStairs, which takes an integer number n as an argument
+and returns how many distinct ways you can climb to the top.
+Ex:
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+"""
+
+def climbStairs(n):
+    if n == 1:
+        return 1
+    
+    if n == 2:
+        return 2
+    
+    else:
+        return climbStairs(n-1) + climbStairs(n - 2)
+
+print(climbStairs(5))
+   
