e06bc2ee2519db5eb8ff805123193b322a82d1a0
Annablaa <128103799+Annablaa@users.noreply.github.com>
2023-07-12 01:22:53 +0400

    bsh

--- CorrectCode/mini/hw9.py
+++
@@ -1,8 +0,0 @@
-'''Write a function 'oddCounter' which takes a list of numbers as an argument and counts how many odd numbers are in it.
-You can only use lambda functions and HOFs: takewhile, dropwhile, zip, filter, map, reduce, enumerate, any, all, sum.
-You are not allowed to use for and while loops.'''
-
-def oddCounter(lst):
-    odds = list(filter(lambda x: x %2 != 0, lst))
-    return len(odds)
-print(oddCounter([1,2,3,4,5,6]))
